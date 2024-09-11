7ac641045b78c888521bec4f8b14e846f3dd3603
VictorEne-Codes <omalevicene@gmail.com>
2024-09-10 17:55:49 -0700

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for a in range(0, 10):
     for b in range(a + 1, 10):
         if not (a == 8 and b == 9):
-            print("{}{}, ".format(a,b), end='')
+            print("{}{}, ".format(a, b), end='')
         else:
-            print("{}{}".format(a,b))
+            print("{}{}".format(a, b))
