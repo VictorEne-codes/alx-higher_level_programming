5e73a4f693df04cbbccf62541e47fbcd80920964
VictorEne-Codes <omalevicene@gmail.com>
2024-09-12 11:29:49 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+ac = len(sys.argv) - 1
+if ac == 1:
+    print("{} argument".format(ac))
+elif ac == 0:
+    print("{} arguments.".format(ac))
+else:
+    print("{} arguments:".format(ac))
+for i in range(1, len(sys.argv)):
+    print("{}: {}".format(i, sys.argv[i]))
