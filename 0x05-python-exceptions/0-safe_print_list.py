4f1f46f1ff4cf2796dca05ed4b577493c770f55c
VictorEne-Codes <omalevicene@gmail.com>
2024-09-30 12:01:54 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
         for i in range(x):
             print("{}".format(int(my_list[i])), end="")
-        print()
+            count += 1
     except IndexError:
-        print("out of range")
-    except ValueError:
-        print("not converted")
-    except TypeError:
-        print("Not an integer")
+        pass
+    print()
+    return count
