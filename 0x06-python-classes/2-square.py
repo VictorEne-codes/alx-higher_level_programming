33e173fc198930c362aafe818432702fd610148d
VictorEne-Codes <omalevicene@gmail.com>
2024-10-02 23:12:11 -0700

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Define class suare with init size"""
 
+
 class Square:
     """defineing a class square"""
     def __init__(self, size=0):
         """initialize self"""
-        self._size = size
+        self.__size = size
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
