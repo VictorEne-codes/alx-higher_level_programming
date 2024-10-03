779f795af083e04a1e88b95a2f14dcb21621ce42
VictorEne-Codes <omalevicene@gmail.com>
2024-10-02 22:24:47 -0700

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+class Square:
+    """This is an empty class that will be passed silently"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This directory contains all tasks for Pythone - Classes and objects
