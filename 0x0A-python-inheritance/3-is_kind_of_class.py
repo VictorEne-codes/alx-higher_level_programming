c0def1b13027969374750e0a76b0ab5484907856
VictorEne-Codes <omalevicene@gmail.com>
2024-10-14 16:43:34 -0700

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function to check if its an instance of class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if it is an ibstance of class
+
+    Args:
+        obj: input
+        a_class: input
+
+    Returns:
+        Boolean: true or false
+    """
+
+    return isinstance(obj, a_class)
