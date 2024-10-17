d131149874c86a292329ec8224903873d8159c61
VictorEne-Codes <omalevicene@gmail.com>
2024-10-17 01:28:04 -0700

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function to append to file"""
+
+
+def append_file(filename="", text=""):
+    """funtion to write"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
