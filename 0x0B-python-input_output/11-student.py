6db21b205b4e1bc04cb9cca7d6001484b0cb555f
VictorEne-Codes <omalevicene@gmail.com>
2024-10-17 10:30:47 -0700

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""create a class student"""
+
+
+class Student:
+    """create a class"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialise class
+
+        Args:
+            first_name: input
+            ladt_name: input
+            age: input
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """from json to class"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for item in attrs:
+            try:
+                new_dict[item] = self.__dict__[item]
+            except Exception:
+                pass
+        return new_dict
+
+    def reload_from_json(self, json):
+        """rwload frlm json"""
+        self.__dict__.update(json)
