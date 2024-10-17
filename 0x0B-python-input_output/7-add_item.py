5bd401da600ff0c48d0bf824a5e997cba769f7af
VictorEne-Codes <omalevicene@gmail.com>
2024-10-17 09:58:15 -0700

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""adds element yo python list"""
+from sys import argv
+
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    new_list = load('add_item.json')
+except (TypeError, ValueError):
+    new_list = []
+
+new_list.extend(argv[1:])
+save(new_list, 'add_item.json')
