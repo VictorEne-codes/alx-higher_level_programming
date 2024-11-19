440cf341989d3d1c365f2820f3e6d8454b086835
VictorEne-Codes <omalevicene@gmail.com>
2024-11-19 13:21:05 -0800

    task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- selects all rows 
-SELECT * FROM second_table WHERE name IS NOT NULL AND name != '';
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
