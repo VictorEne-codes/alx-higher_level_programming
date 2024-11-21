b49935b0564143547f2367fdcf10f7554a59e525
VictorEne-Codes <omalevicene@gmail.com>
2024-11-20 16:26:04 -0800

    task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- select california for database
-SELECT * FROM states WHERE name = 'California' ORDER BY (SELECT MIN(cities.id) FROM cities WHERE cities.state_id = states.id) ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California';
