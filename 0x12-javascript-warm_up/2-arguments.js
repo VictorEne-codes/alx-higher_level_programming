1e1c2d2836192daff19fa43faf9b192f6889b4d3
victor <omalevicene@gmail.com>
2024-12-17 20:54:43 +0000

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
 const args = process.argv;
-if (args.length === 2) {
+if (args.length < 3) {
   console.log('No arguments');
 } else if (args.length === 3) {
   console.log('Argument found');
