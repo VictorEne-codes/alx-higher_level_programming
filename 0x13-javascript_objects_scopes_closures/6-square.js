9eb7a8e18f5f605e2eb4466c6e8845f7d6096a9a
victor <omalevicene@gmail.com>
2025-01-02 23:20:37 +0000

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,48 +1,21 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
+const square = require('./5-square');
+class Square extends square {
   /**
-   * @property {method} print - prints the rectangle using the character X
+   * @property {method} charPrint - this methos will print a square
    * @returns void
    */
-  print () {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
     for (let i = 0; i < this.height; i++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
-        s += 'X';
+        s += c;
       }
       console.log(s);
     }
   }
-
-  /**
-   * @property {method} rotate - this function will exchange w and h
-   * @returns void
-   */
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  /**
-   * @property {method} double - this methos will display multiples of w and h
-   * @returns void
-   */
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
 }
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-}
+module.exports = Square;
